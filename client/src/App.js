9f77b12980d65830a7917e7f58b154e0022b96b8
maxfukuh4ra <maxfuku04@g.ucla.edu>
2024-03-04 17:10:08 -0800

    added server dependencies

--- client/src/App.js
+++ client/src/App.js
@@ -2,7 +2,7 @@
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 
 // pages
-import Home from "./pages/Home";
+import Home from "./Pages/Home";
 
 // components
 import Navbar from "./components/Navbar"
