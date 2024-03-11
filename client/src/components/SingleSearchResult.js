769d6799f9fe967df0e6252250a5bbb082034dc2
JensonW7 <jenson.wong.hk@gmail.com>
2024-03-11 15:52:45 -0700

    added unfollow functionality to search users page

--- client/src/components/SingleSearchResult.js
+++ client/src/components/SingleSearchResult.js
@@ -3,22 +3,45 @@ import { useUserContext } from '../hooks/useUserContext'
 
 const SingleProfile = ({profile, friends, setFriends}) => {
     const {username, accessToken, dispatch} = useUserContext()
-    const handleClick = async (userToFollow) => {
-        const response = await fetch('/users/' + username, {
-            method: 'PATCH',
-            headers: {
-                'Content-Type': 'application/json'
-            },
-            body: JSON.stringify({
-                user: userToFollow
+    const handleClick = (user) => {
+        const unfollowUser = async (user) => {
+            console.log('called unfollow user')
+            const response = await fetch('/users/unfollow/' + username, {
+                method: 'PATCH',
+                headers: {
+                    'Content-Type': 'application/json'
+                },
+                body: JSON.stringify({
+                    user: user
+                })
             })
-        })
+        }
+
+        const followUser = async (user) => {
+            console.log('called follow user')
+            const response = await fetch('/users/' + username, {
+                method: 'PATCH',
+                headers: {
+                    'Content-Type': 'application/json'
+                },
+                body: JSON.stringify({
+                    user: user
+                })
+            })
+        } 
+
+        if (friends && friends.includes(profile.username)) {
+            unfollowUser(user)
+        } else {
+            followUser(user)
+        }
     }
 
     return (
         <div className="profile">
             <button key={profile._id} onClick={() => {handleClick(profile.username)}}>{profile.username}
-                {friends && friends.includes(profile.username) && <span>following</span>}
+                {friends && friends.includes(profile.username) && <span>unfollow</span>}
+                {friends && !friends.includes(profile.username) && <span>follow</span>}
             </button>
         </div>
     )
