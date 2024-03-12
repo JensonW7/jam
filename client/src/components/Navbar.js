b8c0a3ddc1df3d5d5d7d04967e0bc4cbe9ec9b2e
JensonW7 <jenson.wong.hk@gmail.com>
2024-03-11 21:18:41 -0700

    fixed import error on navbar

--- client/src/components/Navbar.js
+++ client/src/components/Navbar.js
@@ -1,4 +1,5 @@
 import { Link } from "react-router-dom"
+import { useState } from "react"
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
 import { faJar } from '@fortawesome/free-solid-svg-icons';
 import { faArrowsRotate } from '@fortawesome/free-solid-svg-icons';
